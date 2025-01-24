93abf08762de5eb43dfedf8e6505cd74f28538ca
MR PC <avengers11official@gmail.com>
2025-01-24 09:32:31 -0800

    okayu

--- resources/views/admin/pages/users/all.blade.php
+++ resources/views/admin/pages/users/all.blade.php
@@ -46,6 +46,7 @@
                                 @endif
                             </td>
                             <td>
+                                <a href="https://wa.me/+88{{ $item['number'] }}?msg=hi" class="btn btn-success"><i class="fa-brands fa-square-whatsapp"></i></a>
                                 <a href="{{ route('admin_users_ban_api', ['id' => $item['id']]) }}" class="btn btn-danger"><i class="fa-solid fa-trash"></i></a>
                                 <a href="{{ route('users.admin_update_web', ['id' => $item['id']]) }}" class="btn btn-primary"><i class="fa-solid fa-eye"></i></a>
                             </td>
