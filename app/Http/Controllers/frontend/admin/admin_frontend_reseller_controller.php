1bc2006c6446242455a1d76e63cb678d4f98747c
MR PC <avengers11official@gmail.com>
2025-01-24 12:57:44 -0800

    okayu

--- app/Http/Controllers/frontend/admin/admin_frontend_reseller_controller.php
+++ app/Http/Controllers/frontend/admin/admin_frontend_reseller_controller.php
@@ -11,7 +11,6 @@ class admin_frontend_reseller_controller extends Controller
     //admin_reseller_all_controller
     public function admin_reseller_all_controller(Request $req)
     {
-        users::where('expired', '<', time()) -> where('role', '0') -> delete();
         $userData = users::orderBy('id', 'DESC') -> where('st', 'active') -> where('role', '2') -> paginate(10);
         return view('admin.pages.reseller.all') -> with(compact('userData'));
     }
@@ -19,7 +18,6 @@ class admin_frontend_reseller_controller extends Controller
     //admin_reseller_ban_controller
     public function admin_reseller_ban_controller(Request $req)
     {
-        users::where('expired', '<', time()) -> where('role', '0') -> delete();
         $userData = users::orderBy('id', 'DESC') -> where('st', 'ban') -> where('role', '2') -> paginate(10);
         return view('admin.pages.reseller.ban') -> with(compact('userData'));
     }
